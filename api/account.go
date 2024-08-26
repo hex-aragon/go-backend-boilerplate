01af132847881e602aa20d0a4cbb97a6829e91e4
hex-aragon <0xaragon90@gmail.com>
2024-08-26 18:29:12 +0900

    ADD: put, delete api

--- api/account.go
+++ api/account.go
@@ -14,15 +14,23 @@ type createAccountRequest struct {
 	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
 }
 
+type putAccountRequest struct { 
+	ID      int64 `uri:"id" 	  binding:"required"`
+	Balance int64 `json:"balance" binding:"required"`
+}
+
 type getAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required"`
 }
 
 type listAccountRequest struct {
 	PageID int32 	`form:"page_id"   binding:"required,min=1"`
 	PageSize int32  `form:"page_size" binding:"required,min=5,max=10"`
 }
-
 
 func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
@@ -50,6 +58,30 @@ func (server *Server) createAccount(ctx *gin.Context) {
 }
 
 
+func (server *Server) putAccount(ctx *gin.Context) {
+	var req putAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return 
+	}
+
+	log.Println("req",&req)
+
+	arg := db.UpdateAccountParams{
+		ID: req.ID,
+		Balance: req.Balance,
+	}
+	log.Println("arg",arg)
+
+	account, err := server.store.UpdateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return 
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
 
 func (server *Server) getAccount(ctx *gin.Context) {
 	var req getAccountRequest
@@ -74,6 +106,29 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, account)
 }
 
+
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return 
+	}
+
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		//내부 DB 에러 
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return 
+		}
+
+		//내부 HTTP 에러 
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return 
+	}
+
+	ctx.JSON(http.StatusOK,"delete ok")
+}
 
 func (server *Server) listAccount(ctx *gin.Context) {
 	var req listAccountRequest
--- api/server.go
+++ api/server.go
@@ -17,6 +17,8 @@ func NewServer(store *db.Store) *Server{
 	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
+	router.PUT("/accounts/:id", server.putAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts/", server.listAccount)
 
