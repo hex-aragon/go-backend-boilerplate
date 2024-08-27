5ed0d70f2c4e7e2ca0bc65e7778d78be4ff4e876
hex-aragon <0xaragon90@gmail.com>
2024-08-27 12:05:28 +0900

    ADD: go viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/hex-aragon/go-backend-boilerplate/util"
 	_ "github.com/lib/pq"
 )
 
@@ -13,15 +14,15 @@ var testQueries *Queries
 var testDB *sql.DB
 
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:boiler@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
 	var err error 
+	
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:",err )
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db",err)
 	}
--- main.go
+++ main.go
@@ -7,17 +7,19 @@ import (
 
 	"github.com/hex-aragon/go-backend-boilerplate/api"
 	db "github.com/hex-aragon/go-backend-boilerplate/db/sqlc"
+	"github.com/hex-aragon/go-backend-boilerplate/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:boiler@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db",err)
 	}
@@ -29,7 +31,7 @@ func main() {
 	fmt.Println("store",store)
 	fmt.Println("server",server)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
