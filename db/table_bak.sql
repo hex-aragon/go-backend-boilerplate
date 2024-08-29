e30951834f109dfdac5a6604f2f1436798453219
hex-aragon <0xaragon90@gmail.com>
2024-08-29 17:06:48 +0900

    ADD: migrate test

--- db/table_bak.sql
+++ db/table_bak.sql
@@ -1,13 +1,5 @@
-CREATE TABLE "users" (
-  "username" varchar PRIMARY KEY,
-  "hashed_password" varchar NOT NULL,
-  "full_name" varchar UNIQUE NOT NULL,
-  "password_changed_at" timestamptz NOT NULL DEFAULT (0001-01-01 00:00:00Z),
-  "created_at" timestamp NOT NULL DEFAULT (now())
-);
-
 CREATE TABLE "accounts" (
-  "id" BIGSERIAL PRIMARY KEY,
+  "id" bigserial PRIMARY KEY,
   "owner" varchar NOT NULL,
   "balance" bigint NOT NULL,
   "currency" varchar NOT NULL,
@@ -15,22 +7,20 @@ CREATE TABLE "accounts" (
 );
 
 CREATE TABLE "entries" (
-  "id" BIGSERIAL PRIMARY KEY,
+  "id" bigserial PRIMARY KEY,
   "account_id" bigint,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
-  "id" BIGSERIAL PRIMARY KEY,
+  "id" bigserial PRIMARY KEY,
   "from_account_id" bigint NOT NULL,
   "to_account_id" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE INDEX ON "accounts" ("owner");
-
-CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
 
 CREATE INDEX ON "entries" ("account_id");
 
@@ -43,8 +33,6 @@ CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 COMMENT ON COLUMN "entries"."amount" IS 'can be negative';
 
 COMMENT ON COLUMN "transfers"."to_account_id" IS 'must be positive';
-
-ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
 
 ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
