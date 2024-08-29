a09192111a06e93530c3da6a886600eb6fc3a617
hex-aragon <0xaragon90@gmail.com>
2024-08-29 12:05:01 +0900

    ADD : testcases, gomock

--- api/account_test.go
+++ api/account_test.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -44,9 +45,49 @@ func TestGetAccountAPI(t *testing.T) {
 			},
 		},
 		// TODO: add more cases
+		{
+			name:       "NotFound",
+			accountID:  account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(db.Account{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name:       "InternalError",
+			accountID:  account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(db.Account{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name:       "InvalidID",
+			accountID:  0,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+
 	}
 
-	for _, tc := range testCases {
+	for i := range testCases {
+		tc := testCases[i]
 		t.Run(tc.name, func(t *testing.T) {
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
@@ -59,7 +100,7 @@ func TestGetAccountAPI(t *testing.T) {
 			recorder := httptest.NewRecorder()
 
 			fmt.Println("account.ID",account.ID)
-			url := fmt.Sprintf("/accounts/%d", account.ID)
+			url := fmt.Sprintf("/accounts/%d", tc.accountID)
 			request, err := http.NewRequest(http.MethodGet, url, nil)
 			require.NoError(t, err)
 
@@ -79,20 +120,13 @@ func randomAccount() db.Account {
 }
 
 func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account db.Account) {
-	fmt.Println("body",body)
-	
+
 	data ,err := io.ReadAll(body)
 	require.NoError(t, err)
-	fmt.Println("data",data)
-	
+
 	var gotAccount db.Account 
 	err = json.Unmarshal(data , &gotAccount)
-	// fmt.Println("t",t)
-	// fmt.Println("err",err)
-	fmt.Println("err",err)
-
-	fmt.Println("account",account)
-	fmt.Println("gotAccount",gotAccount)
+	
 	require.NoError(t, err )
 	require.Equal(t, account, gotAccount)
 
