f22578b8d49f833b0936a87ec2ec65419fe4f4ac
hex-aragon <0xaragon90@gmail.com>
2024-09-10 00:34:27 +0900

    ADD: migrate up, down

--- db/migration/000002_add_users.down.sql
+++ db/migration/000002_add_users.down.sql
@@ -1,16 +1,5 @@
-CREATE TABLE "users" (
-  "username" varchar PRIMARY KEY,
-  "hashed_password" varchar NOT NULL,
-  "full_name" varchar UNIQUE NOT NULL,
-  "password_changed_at" timestamptz NOT NULL DEFAULT (0001-01-01 00:00:00Z),
-  "created_at" timestamp NOT NULL DEFAULT (now())
-);
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "owner_currency_key";
 
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_owner_fkey";
 
-ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
-
-ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
-
-ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
-
-ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+DROP TABLE IF EXISTS users;
--- db/table.sql
+++ db/table.sql
@@ -1,28 +1,29 @@
 CREATE TABLE "users" (
   "username" varchar PRIMARY KEY,
   "hashed_password" varchar NOT NULL,
-  "full_name" varchar UNIQUE NOT NULL,
-  "password_changed_at" timestamptz NOT NULL DEFAULT (0001-01-01 00:00:00Z),
-  "created_at" timestamp NOT NULL DEFAULT (now())
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "accounts" (
-  "id" BIGSERIAL PRIMARY KEY,
+  "id" bigserial PRIMARY KEY,
   "owner" varchar NOT NULL,
   "balance" bigint NOT NULL,
   "currency" varchar NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (now())
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "entries" (
-  "id" BIGSERIAL PRIMARY KEY,
-  "account_id" bigint,
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
-  "id" BIGSERIAL PRIMARY KEY,
+  "id" bigserial PRIMARY KEY,
   "from_account_id" bigint NOT NULL,
   "to_account_id" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
@@ -40,7 +41,7 @@ CREATE INDEX ON "transfers" ("to_account_id");
 
 CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
-COMMENT ON COLUMN "entries"."amount" IS 'can be negative';
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
 
 COMMENT ON COLUMN "transfers"."to_account_id" IS 'must be positive';
 
