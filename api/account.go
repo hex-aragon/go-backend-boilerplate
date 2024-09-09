11ab43da3cc18508ab33918cab2e978143786178
hex-aragon <0xaragon90@gmail.com>
2024-09-10 08:11:57 +0900

    ADD: go handle error

--- api/account.go
+++ api/account.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	db "github.com/hex-aragon/go-backend-boilerplate/db/sqlc"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct { 
@@ -50,6 +51,16 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation","unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return 
+			}
+			log.Println(pqErr.Code.Name())
+		}
+
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return 
 	}
