e313ea4672a54f301c223080d72da65fdb72df19
hex-aragon <0xaragon90@gmail.com>
2024-09-10 07:56:54 +0900

    ADD: user test code

--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -27,8 +27,17 @@ type Entry struct {
 type Transfer struct {
 	ID            int64 `json:"id"`
 	FromAccountID int64 `json:"from_account_id"`
-	ToAccountID   int64 `json:"to_account_id"`
 	// must be positive
-	Amount    int64     `json:"amount"`
-	CreatedAt time.Time `json:"created_at"`
+	ToAccountID int64     `json:"to_account_id"`
+	Amount      int64     `json:"amount"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+type User struct {
+	Username          string    `json:"username"`
+	HashedPassword    string    `json:"hashed_password"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
 }
